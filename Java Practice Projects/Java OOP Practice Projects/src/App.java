5fa4d21f06f44704ac13fa181cb420bd50afb979
Developer <eyob.hg08@gmail.com>
2025-01-17 14:59:40 +0300

    Add new java project and documentation

---
+++ Java Practice Projects/Java OOP Practice Projects/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi There! Enter any number." );
+        String str = sc.nextLine();
+        System.out.println("You entered: " + str);
+        sc.close();
+
+
+    }
+}
+/***
+ * This is a java documentation!
+ */
